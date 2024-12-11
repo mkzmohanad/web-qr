bb4dfd87d2469bc04b06692a6a768a0ff2fb9450
mkzmohanad <mkzmohanad@gmail.com>
2024-12-11 20:49:12 +0200

    TeamQR-fifth

--- src/Home.jsx
+++ src/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
 
     return <div className="flex flex-col items-center justify-center h-dvh">
 
-        <img className="mt-72 sm:mt-0" src="src/assets/graduation-logo-removebg-preview.png" alt="" width={450} height={250} data-aos="zoom-in"/>
+        <img className="mt-72 sm:mt-0" src="./assets/graduation-logo-removebg-preview.png" alt="" width={450} height={250} data-aos="zoom-in"/>
 
         <h1 className="font-semibold text-center text-3xl text-textColor italic tracking-wide leading-10 w-full md:w-1/2" data-aos="fade-up">ArabTutor AI aims to enhance accessibility and inclusivity in education by providing educational resources in Arabic, while also optimizing time efficiency through its content summarization feature.</h1>
 
