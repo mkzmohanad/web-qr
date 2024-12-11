037b7aca9e675669797d4786fb61f2eb937439d2
mkzmohanad <mkzmohanad@gmail.com>
2024-12-11 20:40:11 +0200

    TeamQR-second

--- src/Card.jsx
+++ src/Card.jsx
@@ -5,7 +5,7 @@ import { FaLinkedin } from "react-icons/fa6";
 // eslint-disable-next-line react/prop-types
 function Card({img , name , role , cardWidth , facebook , linkedIn , transition}) {
     return  <div className={`bg-aboutColor xl:w-${cardWidth} w-full px-12 py-20 rounded-2xl text-white flex flex-col items-center justify-center gap-6`}  data-aos={transition}>
-        <img className="rounded-full w-48 h-48" src={`/src/assets/${img}`} alt=""/>
+        <img className="rounded-full w-48 h-48" src={`./src/assets/${img}`} alt=""/>
         <h1 className="text-3xl font-bold capitalize text-center">{name}</h1>
         <h3 className="text-2xl font-semibold italic capitalize">{role}</h3>
         <div className="flex gap-4 text-3xl ">
--- src/Team.jsx
+++ src/Team.jsx
@@ -1,7 +1,5 @@
 import Card from "./Card";
 
-import { useEffect } from "react";
-import AOS from "aos";
 import "aos/dist/aos.css";
 import NextPage from "./NextPage";
 
