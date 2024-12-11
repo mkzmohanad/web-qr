854ed54fd0163abd818fed5fc826bd6f686c01b9
mkzmohanad <mkzmohanad@gmail.com>
2024-12-11 21:28:40 +0200

    test again

--- src/About.jsx
+++ src/About.jsx
@@ -19,12 +19,12 @@ function About() {
 
     return <div className="h-full px-2 sm:p-10 flex xl:flex-row gap-16 justify-between flex-wrap mt-96 md:m-32 lg:mt-10" >
 
-        <div className="bg-aboutColor w-full xl:w-[46%] sm:h-[400px] p-5 rounded-xl text-white" data-aos="fade-right" data-aos-easing="ease-in-sine">
+        <div className="bg-aboutColor w-full xl:w-[46%] sm:h-[400px] p-5 rounded-xl text-white" data-aos="fade-right">
             <h1 className="text-5xl font-semibold py-5 flex items-center gap-3">Vision <FaEye /> </h1>
             <p className="text-xl italic tracking-wide">To create a world where language is no longer a barrier to education, empowering Arabic-speaking learners to access and excel in global academic resources with ease and clarity.</p>
         </div>
 
-        <div className="bg-aboutColor w-full xl:w-[46%] sm:h-[400px] p-5 rounded-xl text-white" data-aos="fade-left" data-aos-easing="ease-in-sine">
+        <div className="bg-aboutColor w-full xl:w-[46%] sm:h-[400px] p-5 rounded-xl text-white" data-aos="fade-left">
             <h1 className="text-5xl font-semibold py-5 flex items-center gap-3">Mission <FaTasks /> </h1>
             <p className="text-xl italic tracking-wide">Our mission is to harness advanced AI technologies to provide high-quality, accessible, and engaging educational content in Arabic. Through real-time video translation, dubbing, and summarization, we aim to enhance the learning experience, bridge linguistic gaps, and support students and educators in achieving their educational goals.</p>
         </div>
