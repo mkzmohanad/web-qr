912c7155baa583657811141d5f7bbfe80ae2b6ff
mkzmohanad <mkzmohanad@gmail.com>
2025-02-26 02:12:27 +0200

    Team-QR-Code

--- src/FirstDiscussion.jsx
+++ src/FirstDiscussion.jsx
@@ -11,9 +11,9 @@ function FirstDiscussion() {
 
     return  <section className="h-fit ">
         <div className="flex flex-col items-center gap-12 mt-16  text-textColor text-center">
-            <h1 className="text-2xl font-bold capitalize" data-aos="zoom-in">{`We are happily announcing that we have completed the first project graduation's discussion at: `} <span className=" line-through">2024-10-29</span> </h1>
+            {/* <h1 className="text-2xl font-bold capitalize" data-aos="zoom-in">{`We are happily announcing that we have completed the first project graduation's discussion at: `} <span className=" line-through">2024-10-29</span> </h1>
 
-            <h3 className="text-2xl italic font-semibold"data-aos="zoom-in">{`Presentation used in the first project graduation's discussion: `} <a className="underline" href="https://prezi.com/view/231g9OkkJSPMtIJ2pzXB/" target="_blank">Team Presentation</a></h3>
+            <h3 className="text-2xl italic font-semibold"data-aos="zoom-in">{`Presentation used in the first project graduation's discussion: `} <a className="underline" href="https://prezi.com/view/231g9OkkJSPMtIJ2pzXB/" target="_blank">Team Presentation</a></h3> */}
 
             <img className="w-full h-[1000px]" src="/assets/team.jpg" alt="" data-aos="fade-up"/>
         </div>
